35a7317d38a71057f3d0f0c0f201383b6b85d126
kevinbuch <kevaustinbuch@gmail.com>
2013-07-19 10:11:54 -0500

    tweaked some css

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ user = User.create(name:'JohnDoe', email:'john@john.com', password:'johnd', pass
 65.times do
   creator = [user.id, nil, nil, nil].sample
   tags = Array.new(rand(3..5)) { Tag.find_or_create_by_title(Faker::Company.buzzwords.sample)}
-  Post.create(title: Faker::Lorem.words.join(' '), body: Faker::Lorem.paragraphs.join(' '), tags: tags, user_id: creator)
+  Post.create(title: Faker::Lorem.words.join(' ').capitalize, body: Faker::Lorem.paragraphs.join(' '), tags: tags, user_id: creator)
 end

697a5ff74e6b1422cb8f602f66169422683f8a30
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 20:33:45 -0500

    added pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'faker'
-
-20.times do
-  Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraphs.join(' '))
+user = User.create(name:'JohnDoe', email:'john@john.com', password:'johnd', password_confirmation:'johnd')
+65.times do
+  creator = [user.id, nil, nil, nil].sample
+  tags = Array.new(rand(3..5)) { Tag.find_or_create_by_title(Faker::Company.buzzwords.sample)}
+  Post.create(title: Faker::Lorem.words.join(' '), body: Faker::Lorem.paragraphs.join(' '), tags: tags, user_id: creator)
 end
